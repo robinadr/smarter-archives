586da60c240d3b2cbba7dff68d79026bd5527d1e
rob1n <rob1n@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-09-16 17:55:00 +0000

    Add APL 2.0 license
    git-svn-id: http://plugins.svn.wordpress.org/smarter-archives/trunk@18980 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smarter-archives.php
+++ smarter-archives.php
@@ -4,8 +4,23 @@ Plugin Name: Smarter Archives
 Plugin URI: http://wordpress.org/extend/plugins/smarter-archives/
 Author: rob1n
 Author URI: http://robinadr.com/
-Description: A unique way of displaying month links by year. Based on <a href="http://justinblanton.com/projects/smartarchives/">original code by Justin Blanton</a>, but much enhanced.
+Description: A unique way of displaying month links by year. Based on <a href="http://justinblanton.com/projects/smartarchives/">original code by Justin Blanton</a>, but much enhanced. Licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0">APL 2.0</a>.
 Version: 1.0
+
+	Copyright 2007 Robin Adrianse a.k.a. rob1n
+	
+	Licensed under the Apache License, Version 2.0 (the "License"); 
+	you may not use this file except in compliance with the License. 
+	You may obtain a copy of the License at
+	
+		http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software 
+	distributed under the License is distributed on an "AS IS" BASIS, 
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+	See the License for the specific language governing permissions and 
+	limitations under the License.
+
 */
 
 function wp_smart_archives()
