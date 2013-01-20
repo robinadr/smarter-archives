30d7266cc1a745da489e182b25d90c8d26dc1647
rob1n <rob1n@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-20 05:34:49 +0000

    Initial 2.0 commit for testing
    git-svn-id: http://plugins.svn.wordpress.org/smarter-archives/trunk@655561 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smarter-archives.php
+++ smarter-archives.php
@@ -2,58 +2,90 @@
 /*
 Plugin Name: Smarter Archives
 Plugin URI: http://wordpress.org/extend/plugins/smarter-archives/
-Author: rob1n
+Author: Robin A.
 Author URI: http://robinadr.com/
-Description: A unique way of displaying month links by year. Based on <a href="http://justinblanton.com/projects/smartarchives/">original code by Justin Blanton</a>, but much enhanced. Licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0">APL 2.0</a>.
-Version: 1.5
+Description: A unique way of displaying month links by year. Based on <a href="http://justinblanton.com/projects/smartarchives/">original code by Justin Blanton</a>, but much enhanced.
+Version: 2.0
 	
-	Licensed under the Apache License, Version 2.0 (the "License"); 
-	you may not use this file except in compliance with the License. 
-	You may obtain a copy of the License at
-	
-		http://www.apache.org/licenses/LICENSE-2.0
-	
-	Unless required by applicable law or agreed to in writing, software 
-	distributed under the License is distributed on an "AS IS" BASIS, 
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-	See the License for the specific language governing permissions and 
-	limitations under the License.
+	Smarter Archives plugin for WordPress
+	Copyright (C) 2013 Robin A.
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
 
-function wp_smart_archives()
+function wp_smart_archives( $args = '' )
 {
 	global $wpdb;
 	
-	$years = $wpdb->get_results( "SELECT distinct year(post_date) AS year, count(ID) as posts FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish' GROUP BY year(post_date) ORDER BY post_date DESC" );
+	$options = array(
+		'mode' => 'default', 
+		'wrapper_class' => 'smart-archives', 
+		'wrapper_tag' => 'div', 
+		'year_link_class' => 'year-link', 
+		'year_tag' => 'p', 
+		'month_link_class' => 'month-link', 
+		'month_tag' => 'span', 
+		'empty_month_class' => 'empty-month'
+	);
+	
+	parse_str( $args, $options );
+	
+	$years = $wpdb->get_results(
+		"SELECT DISTINCT YEAR(post_date) AS year, COUNT(ID) as post_count 
+			FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish' 
+			GROUP BY year(post_date) ORDER BY post_date DESC"
+	);
 	
 	if ( empty( $years ) ) {
 		return;
 	}
 	
-	$months_short = apply_filters( 'smarter_archives_months', array( '', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ) );
+	$short_months = array( 
+		'', __( 'Jan', 'smarter-archives' ), __( 'Feb', 'smarter-archives' ), __( 'Mar', 'smarter-archives' ), 
+		__( 'Apr', 'smarter-archives' ), __( 'May', 'smarter-archives' ), __( 'Jun', 'smarter-archives' ), 
+		__( 'Jul', 'smarter-archives' ), __( 'Aug', 'smarter-archives' ), __( 'Sep', 'smarter-archives' ), 
+		__( 'Oct', 'smarter-archives' ), __( 'Nov', 'smarter-archives' ), __( 'Dec', 'smarter-archives' ) 
+	);
+	$short_months = apply_filters( 'smarter_archives_months', $short_months );
 	
-	print '<div class="smart-archives">';
+	if ( $options['wrapper_class'] != '' ) {
+		printf( '<%s class="%s">', $options['wrapper_tag'], $options['wrapper_class'] );
+	} else {
+		printf( '<%s>', $options['wrapper_tag'] );
+	}
 	
 	foreach ( $years as $year ) {
-		print '<p><a class="year-link" href="' . get_year_link( $year->year ) . '">' . $year->year . '</a>: ';
+		printf( '<%s><a class="%s" href="%s">%s</a>', $options['year_tag'], $options['year_link_class'], get_year_link( $year->year ), $year->year );
 		
 		for ( $month = 1; $month <= 12; $month++ ) {
-			if ( (int) $wpdb->get_var( "SELECT COUNT(ID) FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish' AND year(post_date) = '$year->year' AND month(post_date) = '$month'" ) > 0 ) {
-				print '<a href="' . get_month_link( $year->year, $month ) . '">' . $months_short[$month] . '</a>';
+			if ( (int) $wpdb->get)var( "SELECT COUNT(ID) FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish' AND year(post_date) = '$year->year' AND month(post_date) = '$month'" ) > 0 ) {
+				printf( '<%s><a href="%s" title="%s">%s</a></%s>', $options['month_tag'], get_month_link( $year->year, $month ), $short_months[$month], $options['month_tag'] );
 			} else {
-				print '<span class="empty-month">' . $months_short[$month] . '</span>';
+				printf( '<%s class="%s">%s</%s>', $options['month_tag'], $options['empty_month_class'], $short_months[$month], $options['month_tag'] );
 			}
 			
-			if ( $month != 12 ) {
-				print ' ';
+			if ( $month < 12 ) {
+				echo '&nbsp;';
 			}
 		}
 		
-		print '</p>';
+		printf( '</%s>', $options['year_tag'] );
 	}
 	
-	print '</div>';
+	printf( '</%s>', $options['wrapper_tag'] );
 }
 
 ?>
